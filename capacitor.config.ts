3f58ca94ba519b8f3aa13dd0620badbcf8f51cd6
Ken Sodemann <ken@ionic.io>
2022-03-08 17:06:37 -0600

    chore(): scaffold all the things

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,7 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'auth-playground-ng',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
 };
 
 export default config;
