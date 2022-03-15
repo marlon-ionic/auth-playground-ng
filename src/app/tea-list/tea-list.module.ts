c89512ce0996ed8dce3bc4faf20fc9d2b0e77531
Ken Sodemann <ken@ionic.io>
2022-03-14 21:38:44 -0500

    feat(tea): list the teas

--- src/app/tea-list/tea-list.module.ts
+++ src/app/tea-list/tea-list.module.ts
@@ -2,12 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 import { TeaListPageRoutingModule } from './tea-list-routing.module';
 import { TeaListPage } from './tea-list.page';
 
 @NgModule({
-  imports: [IonicModule, CommonModule, FormsModule, ExploreContainerComponentModule, TeaListPageRoutingModule],
+  imports: [IonicModule, CommonModule, FormsModule, TeaListPageRoutingModule],
   declarations: [TeaListPage],
 })
 export class TeaListPageModule {}
--- src/app/tea-list/tea-list.page.ts
+++ src/app/tea-list/tea-list.page.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TeaService } from '@app/core';
+import { Tea } from '@app/models';
+import { map, mergeMap, Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-tea-ist',
   templateUrl: 'tea-list.page.html',
   styleUrls: ['tea-list.page.scss'],
 })
-export class TeaListPage {
-  constructor() {}
+export class TeaListPage implements OnInit {
+  teas$: Observable<Array<Array<Tea>>>;
+  private refresh: Subject<void>;
+
+  constructor(private teaService: TeaService) {
+    this.refresh = new Subject();
+  }
+
+  ngOnInit(): void {
+    this.teas$ = this.refresh.pipe(
+      mergeMap(() => this.teaService.getAll().pipe(map((teas: Array<Tea>) => this.toMatrix(teas))))
+    );
+  }
+
+  ionViewDidEnter() {
+    this.refresh.next();
+  }
+
+  private toMatrix(tea: Array<Tea>): Array<Array<Tea>> {
+    const matrix: Array<Array<Tea>> = [];
+    let row = [];
+    tea.forEach((t) => {
+      row.push(t);
+      if (row.length === 4) {
+        matrix.push(row);
+        row = [];
+      }
+    });
+
+    if (row.length) {
+      matrix.push(row);
+    }
+
+    return matrix;
+  }
 }
